88441d63861496ad9d7cdaac0f15c718826e06b0
wenyichen <chwwyi@163.com>
2024-11-27 22:24:29 +1300

    feat: add new error when db url missing

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,10 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { Pool } from 'pg';
 
+if (!process.env.XATA_DATBASE_URL) {
+    throw new Error('cannot find XATA_DATBASE_URL')
+}
+
 const pool = new Pool({ connectionString: process.env.XATA_DATBASE_URL, max: 20 });
 const db = drizzle(pool);
 

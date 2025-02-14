971225629172d0d7866d8016323402a618a20a09
Wenyi <cwynz@icloud.com>
2025-02-14 18:37:35 +1300

    feat: update Next.js configuration and add dialog component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
-import {Button} from "@/components/ui/button";
-import Link from "next/link";
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
 export default async function Home() {
-    return (
-        <main className='flex flex-col justify-center h-screen items-center gap-6 mx-auto'>
-            <h1 className='text-5xl font-bold'>Invoicipedia</h1>
-            <p>
-                <Button asChild>
-                    <Link href='/dashboard'>Home</Link>
-                </Button>
-            </p>
-        </main>
-    );
+  return (
+    <main className='flex flex-col justify-center h-screen items-center gap-6 mx-auto'>
+      <h1 className='text-5xl font-bold'>Invoicipedia</h1>
+      <p>
+        <Button asChild>
+          <Link href='/dashboard'>Home</Link>
+        </Button>
+      </p>
+    </main>
+  );
 }
